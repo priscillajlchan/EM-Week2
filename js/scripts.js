1a5e4d839f7aa0a1ce8656f6a4801e0cddfcff3f
Priscilla <priscillachan__@hotmail.com>
2013-08-28 12:04:54 -0700

    Changes made on 08.28
    Added Masonery
    Added Slides JS
    Styled the grid!

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 jQuery(document).ready(function($) {
 
+	//to make our grid pretty
+	$('.posts').masonry({
+		itemSelector:'.post'
+	});
+
+	//for the gallery
+	$("#slides").slidesjs({
+		navigation : {
+			effect : "fade"
+		}
+
+	});
 });
 
 
